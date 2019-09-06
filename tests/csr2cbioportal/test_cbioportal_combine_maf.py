a9d67b7333911b80d4ce86a3b9148e3fdfa84811
ewelinagr <ewelina@thehyve.nl>
2019-09-06 15:54:30 +0200

    Change testing framework of cbio_maf mapper

--- tests/csr2cbioportal/test_cbioportal_combine_maf.py
+++ tests/csr2cbioportal/test_cbioportal_combine_maf.py
@@ -30,88 +30,83 @@ def gz_file(file):
         shutil.copyfileobj(f_in, f_out)
 
 
-class CbioCombineMafTest(unittest.TestCase):
+def test_basic_use_case(tmp_path):
+    ngs_dir = tmp_path.as_posix()
+    maf_file_1 = os.path.join(ngs_dir, 'test1.maf')
+    create_tsv_file(maf_file_1, [
+        ['#version 2.4'],
+        ['Hugo_Symbol', 'Tumor_Sample_Barcode', 'Q', 'W'],
+        ['H1', 'A', '1', '2'],
+        ['H2', 'B', '3', '4']
+    ])
+    gz_file(maf_file_1)
+    maf_file_2 = os.path.join(ngs_dir, 'test2.maf')
+    create_tsv_file(maf_file_2, [
+        ['#version 2.4'],
+        ['Hugo_Symbol', 'Tumor_Sample_Barcode', 'W', 'Z'],
+        ['H3', 'C', '5', '6'],
+        ['H4', 'D', '7', '8']
+    ])
+    gz_file(maf_file_2)
+    out_dir = tempfile.mkdtemp()
+    result_maf_file = os.path.join(out_dir, 'result.maf')
 
-    def test_basic_use_case(self):
-        ngs_dir = tempfile.mkdtemp()
-        maf_file_1 = os.path.join(ngs_dir, 'test1.maf')
-        create_tsv_file(maf_file_1, [
-            ['#version 2.4'],
-            ['Hugo_Symbol', 'Tumor_Sample_Barcode', 'Q', 'W'],
-            ['H1', 'A', '1', '2'],
-            ['H2', 'B', '3', '4']
-        ])
-        gz_file(maf_file_1)
-        maf_file_2 = os.path.join(ngs_dir, 'test2.maf')
-        create_tsv_file(maf_file_2, [
-            ['#version 2.4'],
-            ['Hugo_Symbol', 'Tumor_Sample_Barcode', 'W', 'Z'],
-            ['H3', 'C', '5', '6'],
-            ['H4', 'D', '7', '8']
-        ])
-        gz_file(maf_file_2)
-        out_dir = tempfile.mkdtemp()
-        result_maf_file = os.path.join(out_dir, 'result.maf')
+    samples = csr2cbioportal.combine_maf(
+        ngs_dir=ngs_dir,
+        output_file_location=result_maf_file)
 
-        samples = csr2cbioportal.combine_maf(
-            ngs_dir=ngs_dir,
-            output_file_location=result_maf_file)
-
-        self.assertTrue(os.path.exists(result_maf_file))
-        table = read_tsv_file(result_maf_file)
-        self.assertEqual(5, len(table))
-        self.assertEqual(5, len(table[0]))
-        self.assertIn('Hugo_Symbol', table[0])
-        self.assertIn('Tumor_Sample_Barcode', table[0])
-        self.assertIn('Q', table[0])
-        self.assertIn('W', table[0])
-        self.assertIn('Z', table[0])
-        # check returned samples
-        self.assertEqual(4, len(samples))
-        self.assertIn('A', samples)
-        self.assertIn('B', samples)
-        self.assertIn('C', samples)
-        self.assertIn('D', samples)
-
-    def test_skip_system_file(self):
-        ngs_dir = tempfile.mkdtemp()
-        maf_file_1 = os.path.join(ngs_dir, '.test1.maf')
-        create_tsv_file(maf_file_1, [
-            ['Hugo_Symbol', 'Tumor_Sample_Barcode'],
-            ['H1', 'A'],
-            ['H2', 'B']
-        ])
-        gz_file(maf_file_1)
-        out_dir = tempfile.mkdtemp()
-        result_maf_file = os.path.join(out_dir, 'result.maf')
-
-        samples = csr2cbioportal.combine_maf(
-            ngs_dir=ngs_dir,
-            output_file_location=result_maf_file)
-
-        self.assertFalse(os.path.exists(result_maf_file))
-        self.assertEqual(0, len(samples))
-
-    def test_skip_comment_lines(self):
-        ngs_dir = tempfile.mkdtemp()
-        maf_file_1 = os.path.join(ngs_dir, 'test1.maf')
-        create_tsv_file(maf_file_1, [
-            ['Hugo_Symbol', 'Tumor_Sample_Barcode'],
-            ['H1', 'A'],
-            ['#H2', 'B']
-        ])
-        gz_file(maf_file_1)
-        out_dir = tempfile.mkdtemp()
-        result_maf_file = os.path.join(out_dir, 'result.maf')
-
-        samples = csr2cbioportal.combine_maf(
-            ngs_dir=ngs_dir,
-            output_file_location=result_maf_file)
-
-        self.assertTrue(os.path.exists(result_maf_file))
-        self.assertEqual(1, len(samples))
-        self.assertIn('A', samples)
+    assert os.path.exists(result_maf_file)
+    table = read_tsv_file(result_maf_file)
+    assert len(table) == 5
+    assert len(table[0]) == 5
+    assert 'Hugo_Symbol' in table[0]
+    assert 'Tumor_Sample_Barcode' in table[0]
+    assert 'Q' in table[0]
+    assert 'W' in table[0]
+    assert 'Z' in table[0]
+    # check returned samples
+    assert len(samples) == 4
+    assert 'A' in samples
+    assert 'B' in samples
+    assert 'C' in samples
+    assert 'D' in samples
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_skip_system_file(tmp_path):
+    ngs_dir = tmp_path.as_posix()
+    maf_file_1 = os.path.join(ngs_dir, '.test1.maf')
+    create_tsv_file(maf_file_1, [
+        ['Hugo_Symbol', 'Tumor_Sample_Barcode'],
+        ['H1', 'A'],
+        ['H2', 'B']
+    ])
+    gz_file(maf_file_1)
+    result_maf_file = os.path.join(ngs_dir, 'result.maf')
+
+    samples = csr2cbioportal.combine_maf(
+        ngs_dir=ngs_dir,
+        output_file_location=result_maf_file)
+
+    assert not os.path.exists(result_maf_file)
+    assert len(samples) == 0
+
+
+def test_skip_comment_lines(tmp_path):
+    ngs_dir = tmp_path.as_posix()
+    maf_file_1 = os.path.join(ngs_dir, 'test1.maf')
+    create_tsv_file(maf_file_1, [
+        ['Hugo_Symbol', 'Tumor_Sample_Barcode'],
+        ['H1', 'A'],
+        ['#H2', 'B']
+    ])
+    gz_file(maf_file_1)
+    out_dir = tempfile.mkdtemp()
+    result_maf_file = os.path.join(out_dir, 'result.maf')
+
+    samples = csr2cbioportal.combine_maf(
+        ngs_dir=ngs_dir,
+        output_file_location=result_maf_file)
+
+    assert os.path.exists(result_maf_file)
+    assert len(samples) == 1
+    assert 'A' in samples
