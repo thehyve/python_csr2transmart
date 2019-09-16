862b51f1b85982c99731b2d97df973bba4fc7fa1
Spayralbe <stefan@thehyve.nl>
2019-09-16 15:22:03 +0200

    Add click argument verification

--- sources2csr/sources2csr.py
+++ sources2csr/sources2csr.py
@@ -31,9 +31,9 @@ def sources2csr(input_dir, output_dir, config_dir):
 
 
 @click.command()
-@click.argument('input_dir')
-@click.argument('output_dir')
-@click.argument('config_dir')
+@click.argument('input_dir', type=click.Path(file_okay=False, exists=True, readable=True))
+@click.argument('output_dir', type=click.Path(file_okay=False, writable=True))
+@click.argument('config_dir', type=click.Path(file_okay=False, exists=True, readable=True))
 @click.version_option()
 def run(input_dir, output_dir, config_dir):
     sources2csr(input_dir, output_dir, config_dir)
